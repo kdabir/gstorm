496f4e8d56633059fbdf2c36c21b299df703ba8c
Kunal Dabir <kunal.dabir@gmail.com>
2013-04-21 00:43:49 +0530

    add an example for demo

---
+++ examples/getting_started.groovy
@@ -0,0 +1,19 @@
+@GrabResolver(name='gstorm', root='https://raw.github.com/kdabir/mavenrepo/gh-pages/') @Grab('gstorm:gstorm:0.1')
+@GrabConfig(systemClassLoader = true) @Grab('org.hsqldb:hsqldb:2.0.0')
+import groovy.sql.*
+import gstorm.*
+
+def sql = Sql.newInstance("jdbc:hsqldb:mem:database", "sa", "", "org.hsqldb.jdbcDriver")
+
+class Person { String name, project } // this is your model class
+
+def g = new Gstorm(sql)
+g.stormify(Person) // table automatically gets created for this class
+
+def person = new Person(name: "kunal", project: "gstorm")
+person.save() // model automatically gets this method
+
+new Person(name: "test", project: "other").save() // save one more
+
+def result = Person.where("name = 'kunal'") // pass any standard SQL where clause
+println "result -> $result"

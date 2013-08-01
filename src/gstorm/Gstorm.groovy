94a3ed984ee4c0648dbd1ec578a0b0b07d0458b4
Kunal Dabir <kunal.dabir@gmail.com>
2013-08-01 23:25:44 +0530

    Extract out the model enhancer

--- src/gstorm/Gstorm.groovy
+++ src/gstorm/Gstorm.groovy
@@ -3,10 +3,6 @@ package gstorm
 import groovy.sql.Sql
 import groovy.util.logging.Log
 import gstorm.builders.CreateTableQueryBuilder
-import gstorm.builders.DeleteQueryBuilder
-import gstorm.builders.InsertQueryBuilder
-import gstorm.builders.SelectQueryBuilder
-import gstorm.builders.UpdateQueryBuilder
 import gstorm.metadata.ClassMetaData
 
 import java.util.logging.Level
@@ -22,62 +18,13 @@ class Gstorm {
     def stormify(Class modelClass) {
         ClassMetaData classMetaData = new ClassMetaData(modelClass)
         createTableFor(classMetaData)
-        addStaticDmlMethodsTo(classMetaData)
-        addInstanceDmlMethodsTo(classMetaData)
+        new ModelClassEnhancer(classMetaData, sql).enhance()
     }
 
     private def createTableFor(ClassMetaData metaData) {
         sql.execute(new CreateTableQueryBuilder(metaData).build())
     }
 
-    private def addStaticDmlMethodsTo(ClassMetaData metaData) {
-        final modelMetaClass = metaData.modelClass.metaClass
-        def selectAllQuery = new SelectQueryBuilder(metaData).build()
-        def selectByIdQuery = new SelectQueryBuilder(metaData).where("ID = ?").build()
-
-        modelMetaClass.static.where = { clause ->
-            sql.rows(new SelectQueryBuilder(metaData).where(clause).build())
-        }
-
-        modelMetaClass.static.get = { id ->
-            final result = sql.rows(selectByIdQuery, [id])
-            (result) ? result.first() : null
-        }
-
-        def getAll = {
-            sql.rows(selectAllQuery)
-        }
-        // alias
-        modelMetaClass.static.getAll = getAll
-        modelMetaClass.static.all = getAll
-    }
-
-    private def addInstanceDmlMethodsTo(ClassMetaData metaData) {
-        final modelMetaClass = metaData.modelClass.metaClass
-        final fieldNames = metaData.fields*.name
-        final insertQuery = new InsertQueryBuilder(metaData).build()
-        final updateQuery = new UpdateQueryBuilder(metaData).where("id = ?").build()
-        final deleteQuery = new DeleteQueryBuilder(metaData).where("id = ?").build()
-
-        modelMetaClass.id = null // add id
-
-        modelMetaClass.save = {
-            if (delegate.id == null) {
-                final values = fieldNames.collect { delegate.getProperty(it)}
-                final generated_ids = sql.executeInsert(insertQuery, values)
-                delegate.id = generated_ids[0][0]
-            } else {
-                final values = fieldNames.collect { delegate.getProperty(it)} << delegate.id
-                sql.executeUpdate(updateQuery, values)
-            }
-            delegate
-        }
-
-        modelMetaClass.delete = {
-            if (delegate.id != null) { sql.execute(deleteQuery, [delegate.id]) }
-            delegate
-        }
-    }
 
     def enableQueryLogging(level = Level.FINE) {
         def sqlMetaClass = Sql.class.metaClass
---
+++ src/gstorm/ModelClassEnhancer.groovy
@@ -0,0 +1,75 @@
+package gstorm
+
+import groovy.sql.Sql
+import gstorm.builders.DeleteQueryBuilder
+import gstorm.builders.InsertQueryBuilder
+import gstorm.builders.SelectQueryBuilder
+import gstorm.builders.UpdateQueryBuilder
+import gstorm.metadata.ClassMetaData
+
+class ModelClassEnhancer {
+    ClassMetaData metaData
+    Sql sql
+
+    ModelClassEnhancer(ClassMetaData classMetaData, Sql sql) {
+        this.metaData = classMetaData
+        this.sql = sql
+    }
+
+    public void enhance() {
+        addStaticDmlMethods()
+        addInstanceDmlMethods()
+    }
+
+    private def addStaticDmlMethods() {
+        final modelMetaClass = metaData.modelClass.metaClass
+        def selectAllQuery = new SelectQueryBuilder(metaData).build()
+        def selectByIdQuery = new SelectQueryBuilder(metaData).where("ID = ?").build()
+
+        modelMetaClass.static.where = { clause ->
+            sql.rows(new SelectQueryBuilder(metaData).where(clause).build())
+        }
+
+        modelMetaClass.static.get = { id ->
+            final result = sql.rows(selectByIdQuery, [id])
+            (result) ? result.first() : null
+        }
+
+        def getAll = {
+            sql.rows(selectAllQuery)
+        }
+        // alias
+        modelMetaClass.static.getAll = getAll
+        modelMetaClass.static.all = getAll
+    }
+
+    private def addInstanceDmlMethods() {
+        final modelMetaClass = metaData.modelClass.metaClass
+        final fieldNames = metaData.fields*.name
+        final insertQuery = new InsertQueryBuilder(metaData).build()
+        final updateQuery = new UpdateQueryBuilder(metaData).where("id = ?").build()
+        final deleteQuery = new DeleteQueryBuilder(metaData).where("id = ?").build()
+
+        modelMetaClass.id = null // add id
+
+        modelMetaClass.save = {
+            if (delegate.id == null) {
+                final values = fieldNames.collect { delegate.getProperty(it) }
+                final generated_ids = sql.executeInsert(insertQuery, values)
+                delegate.id = generated_ids[0][0]
+            } else {
+                final values = fieldNames.collect { delegate.getProperty(it) } << delegate.id
+                sql.executeUpdate(updateQuery, values)
+            }
+            delegate
+        }
+
+        modelMetaClass.delete = {
+            if (delegate.id != null) {
+                sql.execute(deleteQuery, [delegate.id])
+            }
+            delegate
+        }
+    }
+
+}

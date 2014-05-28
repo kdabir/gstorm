451f092a8929736afa38c8c8dbbf6d064c0b1521
Kunal Dabir <kunal.dabir@gmail.com>
2014-05-28 09:45:41 +0300

    Move test to JUnit4 so that it can be easily ignored :)

--- test/gstorm/GstormPerfTest.groovy
+++ test/gstorm/GstormPerfTest.groovy
@@ -2,12 +2,18 @@ package gstorm
 
 import groovy.sql.Sql
 import models.Person
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
 
-class GstormPerfTest extends GroovyTestCase {
+
+class GstormPerfTest {
     Gstorm gstorm
     Sql sql
     def start, end
 
+    @Before
     void setUp() {
         sql = Sql.newInstance("jdbc:hsqldb:mem:database", "sa", "", "org.hsqldb.jdbc.JDBCDriver")
         gstorm = new Gstorm(sql)
@@ -15,12 +21,14 @@ class GstormPerfTest extends GroovyTestCase {
         start = System.nanoTime()
     }
 
+    @After
     void tearDown() {
         sql.execute("drop table person if exists")
         sql.close()
     }
 
-    void "test the time taken for 1000 inserts"() {
+    @Test //@Ignore
+    void "check the time taken for 1000 inserts"() {
         1000.times {new Person(name: 'Spiderman', age: 30).save()}
         assert sql.rows("select count(*) as total_count from person").total_count == [1000]
         printTimeTakenFor("1000 inserts")

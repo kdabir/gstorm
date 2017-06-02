b68ce7ed2d4f79a2874bc3fc8d12ce9302caa866
Kunal Dabir <kunal.dabir@gmail.com>
2017-06-02 09:26:40 +0530

    the type's type would be Class :)

--- src/gstorm/metadata/FieldMetaData.groovy
+++ src/gstorm/metadata/FieldMetaData.groovy
@@ -5,7 +5,8 @@ import gstorm.helpers.TypeMapper
 import java.lang.reflect.Field
 
 class FieldMetaData {
-    String type, name, columnName, columnType
+    Class type
+    String name, columnName, columnType
 
     FieldMetaData(Field field) {
         this.type = field.type
